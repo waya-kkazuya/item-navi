e060a7e085ce9facd1349a5146b53e09a370743f
waya-kkazuya <k.kazuya.for.application@gmail.com>
2024-05-22 18:15:58 +0900

    ヘッダーにベルSVGアイコン 消耗品行にBarChartのSVGアイコン追加

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -44,18 +44,12 @@ const showingNavigationDropdown = ref(false);
                                 </NavLink>
                                 
                                 <BellNotification />
-                                <div class="relative inline-block">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
-                                    </svg>
-                                    <span class="absolute top-0 right-0">1</span>
-                                </div>
 
                                 <div>
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
                                     </svg>
-                                    <span>3</span>
+                                    <span class="absolute top-0 right-0">3</span>
                                 </div>
                             </div>
                             
