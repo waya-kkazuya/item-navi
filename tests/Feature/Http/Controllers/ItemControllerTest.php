cdc20c6cb580e06a5d9a6d500713e043d17e38ee
waya-kkazuya <k.kazuya.for.application@gmail.com>
2024-06-25 12:04:30 +0900

    test:ログインして備品管理画面表示

--- tests/Feature/Http/Controllers/ItemControllerTest.php
+++ tests/Feature/Http/Controllers/ItemControllerTest.php
@@ -5,13 +5,22 @@ namespace Tests\Feature\Http\Controllers;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
 
 class ItemControllerTest extends TestCase
 {
     /** @test */
     function 備品一覧が表示される()
     {
-        $this->get('items')
+        // 権限レベルが必要な値以上のユーザーを作成
+        $user = User::factory()->create([
+            'role' => '1',
+        ]);
+
+        $this->actingAs($user)->get('items')
             ->assertOk();
+
+
+            
     }
 }
