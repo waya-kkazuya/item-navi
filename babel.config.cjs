39afcbef919c9a21a11254486d1202ab8e0839f7
waya-kkazuya <k.kazuya.for.application@gmail.com>
2024-06-28 17:45:02 +0900

    chore:[Jest]テスト環境の構築 依存関係の問題解消中

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,6 +1,10 @@
 module.exports = {
-  "presets": [
+  presets: [
     "@babel/preset-env",
     "@vue/babel-preset-app"
-  ]
+  ],
+  plugins: [
+    // 他のプラグインがある場合はここに記述します
+    '@vue/babel-plugin-jsx'
+  ],
 };
