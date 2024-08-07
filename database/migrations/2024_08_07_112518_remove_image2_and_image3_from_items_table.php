ffb4d769a92bc2d6f1d6effdb4df06dd68a06d76
waya-kkazuya <k.kazuya.for.application@gmail.com>
2024-08-07 11:27:30 +0900

    fix:Itemsテーブルのimage2とimage3カラムを削除、そのmigrationファイル

---
+++ database/migrations/2024_08_07_112518_remove_image2_and_image3_from_items_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('items', function (Blueprint $table) {
+            $table->dropColumn('image2');
+        });
+
+        Schema::table('items', function (Blueprint $table) {
+            $table->dropColumn('image3');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('items', function (Blueprint $table) {
+            $table->string('image2')->nullable();
+        });
+
+        Schema::table('items', function (Blueprint $table) {
+            $table->string('image3')->nullable();
+        });
+    }
+};
