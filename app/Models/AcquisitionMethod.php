eff9c69585dcac1df820669a1fe8d5f00dca2abc
waya-kkazuya <k.kazuya.for.application@gmail.com>
2024-08-07 20:18:11 +0900

    fix:AquisitionMethodにリレーション追加

--- app/Models/AcquisitionMethod.php
+++ app/Models/AcquisitionMethod.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class AcquisitionMethod extends Model
 {
     use HasFactory;
+
+    public function item() {
+        return $this->hasOne(Item::class);
+    }
 }
