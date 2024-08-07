9afea60101a11fd71e8df2e415443b40883b49be
waya-kkazuya <k.kazuya.for.application@gmail.com>
2024-08-07 20:21:53 +0900

    feat:ItemObserverのcreateing時に使用するmanagement_idを生成するメソッドを追加

--- app/Observers/ItemObserver.php
+++ app/Observers/ItemObserver.php
@@ -8,9 +8,61 @@ use Illuminate\Support\Facades\Auth;
 
 class ItemObserver
 {
-    /**
-     * Handle the Item "created" event.
-     */
+    // storeメソッドのItem::createの呼び出しが行われる前に保存される
+    public function creating(Item $item)
+    {
+        $item->management_id = $this->generateManagementId($item->category_id);
+    }
+
+    private function generateManagementId($category_id)
+    {
+        $prefix = '';
+
+        switch ($category_id) {
+            case 1: // 消耗品
+                $prefix = 'CO';
+                break;
+            case 2 : // IT機器
+                $prefix = 'IT';
+                break;
+            case 3: // ソフトウェアアカウント
+                $prefix = 'SA';
+                break;
+            case 4: // 電化製品
+                $prefix = 'EA';
+                break;
+            case 5: // 防災用品
+                $prefix = 'DP';
+                break;
+            case 6: // オフィス用品
+                $prefix = 'OS';
+                break;
+            case 7: // オフィス家具
+                $prefix = 'OF';
+                break;
+            case 8: // 作業道具
+                $prefix = 'TO';
+                break;
+            case 9: // 清掃用具
+                $prefix = 'CL';
+                break;
+            case 10: // その他
+                $prefix = 'OT';
+                break;
+            default:
+                // カテゴリが無効な場合に例外をスロー
+                throw ValidationException::withMessages(['category_id' => 'カテゴリが不正です']);
+        }
+
+        do {
+            $randomNumber = str_pad(rand(0, 9999), 4, '0', STR_PAD_LEFT);
+            $managementId = $prefix . '-' . $randomNumber;
+        } while (Item::where('management_id', $managementId)->exists());
+
+        return $managementId;
+    }
+
+
     public function created(Item $item): void
     {
         // 注意
@@ -30,9 +82,6 @@ class ItemObserver
         ]);
     }
 
-    /**
-     * Handle the Item "updated" event.
-     */
     public function updated(Item $item): void
     {
         $changes = $item->getChanges();
