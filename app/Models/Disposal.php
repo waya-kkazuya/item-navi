3348bd59eae32f0beaf316da59a445668fe60d9f
waya-kkazuya <k.kazuya.for.application@gmail.com>
2024-08-07 20:18:46 +0900

    feat:Disposalモデル追加

---
+++ app/Models/Disposal.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Disposal extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'item_id',
+        'scheduled_date',
+        'disposal_date',
+        'disposal_person',
+        'details',
+    ];
+
+    public function item()
+    {
+        return $this->belongsTo(Item::class);
+    }
+}
