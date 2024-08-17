957c6000f836fe3fcf0e3678d001fd2ebe8f37cb
waya-kkazuya <k.kazuya.for.application@gmail.com>
2024-08-17 21:06:51 +0900

    chore:stock_transactionsテーブル作成

---
+++ database/migrations/2024_08_17_173503_create_stock_transactions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stock_transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('item_id')->constraind('items');
+            $table->enum('transaction_type', ['入庫', '出庫']);
+            $table->integer('quantity');
+            $table->string('operator_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stock_transactions');
+    }
+};
