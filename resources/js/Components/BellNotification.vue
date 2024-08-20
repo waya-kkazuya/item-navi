d3709cd9939d810dd7e83601fdd6d16539c4ae54
waya-kkazuya <k.kazuya.for.application@gmail.com>
2024-08-20 22:03:45 +0900

    feat:ベルはリンク機能と新着通知の番号のみの実装

--- resources/js/Components/BellNotification.vue
+++ resources/js/Components/BellNotification.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { Link } from '@inertiajs/vue3';
 import { ref, onMounted } from 'vue';
 import axios from 'axios';
 
@@ -8,10 +9,11 @@ const showModal = ref(false);
 
 onMounted(async () => {
   try {
-    await axios.get('/api/notifications')
+    await axios.get('/api/notifications_count')
     .then(res => {
+      console.log('BellNotification')
       console.log(res.data)
-      notifications.value = response.data;
+      notifications.value = res.data
     })
   } catch (e) {
     console.error(e);
@@ -21,25 +23,25 @@ onMounted(async () => {
 
 <template>
   <div class="relative">
+      <!-- <button @click="showModal = !showModal"> -->
+      <Link as="button" :href="route('notifications.index')" class="flex">
+        <svg class="w-7 h-7" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M22 17H2a3 3 0 0 0 3-3V9a7 7 0 0 1 14 0v5a3 3 0 0 0 3 3zm-8.27 4a2 2 0 0 1-3.46 0"></path></svg>
 
-      <button @click="showModal = !showModal">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
-        </svg>
         <span class="absolute top-0 right-0 text-xs bg-blue-500 text-white rounded-full w-4 h-4 flex items-center justify-center">
           {{ notifications.length }}
         </span>
-      </button>
+      </Link>
+      <!-- </button> -->
 
       <!-- モーダルのCSSが重要 -->
-      <div v-if="showModal" class="absolute top-full mt-2 w-64 bg-white rounded shadow-xl overflow-auto max-h-64">
+      <!-- <div v-if="showModal" class="absolute top-full mt-2 w-64 bg-white rounded shadow-xl overflow-auto max-h-64">
         <div v-for="notification in notifications" :key="notification.id">
-            <p>{{ notification.data.item_name }}</p>
+            <p>{{ notification.data.item_name }}</p> -->
             <!-- あとでquantityをstocksに直す -->
-            <p>{{ notification.data.quantity }}</p>
+            <!-- <p>{{ notification.data.quantity }}</p>
             <p>{{ notification.data.message }}</p>
         </div>
-      </div>
+      </div> -->
 
   </div>
 </template>
