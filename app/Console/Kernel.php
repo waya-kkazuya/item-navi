6e369f257a6d4440545afd5f3768531e41d8a3b3
waya-kkazuya <k.kazuya.for.application@gmail.com>
2024-08-20 21:50:54 +0900

    feat:点検予定日と廃棄予定日のスケジューラでの通知準備

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,8 +2,14 @@
 
 namespace App\Console;
 
+
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Models\Inspection;
+use App\Models\Disposal;
+use App\Notifications\InspectionScheduleNotification;
+use App\Notifications\DisposalScheduleNotification;
+use Carbon\Carbon;
 
 class Kernel extends ConsoleKernel
 {
@@ -13,6 +19,10 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            $this->sendInspectionNotifications();
+            $this->sendDisposalNotifications();
+        })->weekdays()->at('08:00');
     }
 
     /**
@@ -24,4 +34,27 @@ class Kernel extends ConsoleKernel
 
         require base_path('routes/console.php');
     }
+
+    
+    protected function sendInspectionNotifications()
+    {
+        $inspections = Inspection::whereDate('scheduled_date', Carbon::today())->get();
+        foreach ($inspections as $inspection) {
+            $users = User::all();
+            foreach ($users as $user) {
+                $user->notify(new InspectionScheduleNotification($inspection));
+            }
+        }
+    }
+
+    protected function sendDisposalNotifications()
+    {
+        $disposals = Disposal::whereDate('scheduled_date', Carbon::today())->get();
+        foreach ($disposals as $disposal) {
+            $users = User::all();
+            foreach ($users as $user) {
+                $user->notify(new DisposalScheduleNotification($disposal));
+            }
+        }
+    }    
 }
---
+++ app/Notifications/DisposalScheduleNotification.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class DisposalScheduleNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($item)
+    {
+        $this->item = $item;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->line('The introduction to the notification.')
+                    ->action('Notification Action', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        $disposal = $this->item->disposal; // リレーションで取得
+
+        return [
+            'item_name' => $this->item->name,
+            'scheduled_date' => $disposal->scheduled_date,
+            'message' => '廃棄予定日が近づいています'
+        ];
+    }
+}
