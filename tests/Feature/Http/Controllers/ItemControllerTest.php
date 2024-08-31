cb1c7069ba603b23e4c8f38f07fc95a254a7eb04
waya-kkazuya <k.kazuya.for.application@gmail.com>
2024-08-31 21:31:22 +0900

    test:Itemの/items画面用テストを作成中

--- tests/Feature/Http/Controllers/ItemControllerTest.php
+++ tests/Feature/Http/Controllers/ItemControllerTest.php
@@ -13,6 +13,7 @@ use App\Models\Location;
 use App\Models\UsageStatus;
 use App\Models\AcquisitionMethod;
 use Faker\Factory as FakerFactory;
+use Inertia\Testing\AssertableInertia as Assert;
 
 class ItemControllerTest extends TestCase
 {
@@ -47,11 +48,17 @@ class ItemControllerTest extends TestCase
         echo 'Locations count: ' . $locations->count() . PHP_EOL;
         echo 'Acquisition Methods count: ' . $aquisition_methods->count() . PHP_EOL;
 
+        // adminユーザーを作成
+        $user = User::factory()->role(1)->create();
+
+        
         // 1件作成
         // Observerを無効にする
-        Item::withoutEvents(function () use ($categories, $units, $usage_statuses, $locations, $aquisition_methods) {
-            $item = Item::factory()->create([
+        $items = null; // 参照渡し
+        Item::withoutEvents(function () use (&$items ,$categories, $units, $usage_statuses, $locations, $aquisition_methods) {
+            $items = Item::factory()->count(20)->create([
                 'management_id' => $this->faker->regexify('[A-Za-z0-9]{7}'),
+                'name' => 'テストアイテム', // nameを上書きできる
                 'category_id' => $categories->random()->id,
                 'unit_id' => $units->random()->id,
                 'usage_status_id' => $usage_statuses->random()->id,
@@ -61,8 +68,62 @@ class ItemControllerTest extends TestCase
             ]);
         });
 
-        $this->get('/')
+        $this->actingAs($user);
+
+        // $items = Item::all();
+        // dd($items);
+
+        // dd($items);
+
+        $response = $this->get('/items')
             ->assertOk();
+
+        // dd($response->getContent());
+
+        $response->assertInertia(fn (Assert $page) => $page
+            ->component('Items/Index') // Vueコンポーネント
+            ->has('items.data', 20)// itemsが1件存在することを確認
+            ->has('items.data', fn ($data) => $data
+                ->each(fn ($item)=> $item
+                    ->hasAll([
+                        'id',
+                        'management_id',
+                        'name',
+                        'category_id',
+                        'image1',
+                        'stock',
+                        'unit_id',
+                        'minimum_stock',
+                        'notification',
+                        'usage_status_id',
+                        'end_user',
+                        'location_of_use_id',
+                        'storage_location_id',
+                        'acquisition_method_id',
+                        'acquisition_source',
+                        'price',
+                        'date_of_acquisition',
+                        'manufacturer',
+                        'product_number',
+                        'remarks',
+                        'qrcode',
+                        'deleted_at',
+                        'created_at',
+                        'image_path1', //画像名から加工した画像パス
+                        'pending_inspection_date',
+                        'category',
+                        'unit',
+                        'usage_status',
+                        'location_of_use',
+                        'storage_location',
+                        'acquisition_method',
+                        'inspections',
+                        'disposal',
+                    ])
+                )
+            )
+            // ->where('items.0.name', 'テストアイテム') // itemsの最初の要素のnameがテストアイテムであることを確認
+        );
 
 
         // // テストデータを生成、テストの世界を構築
@@ -106,4 +167,6 @@ class ItemControllerTest extends TestCase
         //     ->has('storage_location', fn ($location) => $location->hasAll(['id', 'name', 'created_at', 'updated_at']))  // storage_locationオブジェクトがid属性を持っていることを確認
         // )));         
     }
+
+
 }
