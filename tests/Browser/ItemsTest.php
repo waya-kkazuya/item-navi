7160812fb8ce63e6f3c7f868f2ea3f18877f5c2f
waya-kkazuya <k.kazuya.for.application@gmail.com>
2024-08-31 21:32:02 +0900

    test:Laravel Duskのテストを練習

--- tests/Browser/ItemsTest.php
+++ tests/Browser/ItemsTest.php
@@ -5,25 +5,28 @@ namespace Tests\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
+use App\Models\User;
 
 class ItemsTest extends DuskTestCase
 {
     /**
      * A Dusk test example.
      */
-    // public function testExample(): void
-    // {
-    //     $this->browse(function (Browser $browser) {
-    //         $browser->visit('/')
-    //                 ->assertSee('Laravel');
-    //     });
-    // }
+    public function testExample(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->assertSee('Laravel');
+        });
+    }
 
     public function testItemsPageContainsText()
     {
         $this->browse(function (Browser $browser) {
-            $browser->visit('/items')
-                ->assertSee('備品名'); 
+            $browser->loginAs(User::find(1));
+            $browser->visit('/items');
+            $browser->screenshot('filename');
+                // ->assertSee('備品名'); 
                 // ->assertSeeIn('table th', '備品名'); 
         });
     }
