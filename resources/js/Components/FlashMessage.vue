4371504db53c61c2c053e6528e8c6390479510f9
waya-kkazuya <k.kazuya.for.application@gmail.com>
2024-09-21 17:41:47 +0900

    style:FlashMessage.vueのレスポンシブデザインを作成

--- resources/js/Components/FlashMessage.vue
+++ resources/js/Components/FlashMessage.vue
@@ -3,11 +3,11 @@
 
 <template>
   <div v-if="$page.props.flash.status === 'success'" 
-  class="bg-blue-300 text-white p-4">
+  class="bg-blue-300 text-xs md:text-base text-white p-4">
   {{ $page.props.flash.message }}
   </div>
   <div v-if="$page.props.flash.status === 'danger'" 
-  class="bg-red-300 text-white p-4">
+  class="bg-red-300 text-xs md:text-base text-white p-4">
   {{ $page.props.flash.message }}
   </div>
 </template>
