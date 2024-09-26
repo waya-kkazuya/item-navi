036b421391837a7623a2e67ca108100aa8573675
waya-kkazuya <k.kazuya.for.application@gmail.com>
2024-09-26 16:58:37 +0900

    style:CSSの修正

--- resources/js/Pages/ConsumableItems/Index.vue
+++ resources/js/Pages/ConsumableItems/Index.vue
@@ -133,7 +133,7 @@ const fetchConsumableItems = async () => {
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">
                       <FlashMessage />
-                      <section class="text-gray-600 body-font">
+                      <section class="mt-2 text-gray-600 body-font">
                           <div class="container px-5 mx-auto">
                             <!-- ボタンはコンテナ直下 -->
                             <div class="flex justify-center">
