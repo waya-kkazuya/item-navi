318115d99a03fee3dad5a43f20dafad63a52b3f3
waya-kkazuya <k.kazuya.for.application@gmail.com>
2024-10-04 23:19:54 +0900

    feat:PDFのデザイン完成

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Item;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 // use PDF;
@@ -12,6 +13,9 @@ class PDFController extends Controller
 {
     public function generatePDF()
     {
+        // 消耗品のQRコードをすべて取得する（現在カテゴリが消耗品のもの、変更されている可能性もある）
+        // $consumableItems = Item::whereNull('deleted_at');
+
         // QRコード画像のパスを取得
         $qrCodes = [];
         for ($i = 1; $i <= 11; $i++) {
@@ -24,21 +28,18 @@ class PDFController extends Controller
         \Log::info("qrCodes",$qrCodes);
         \Log::info("qrCodes");
 
-        // $qrCodePairs = array_chunk($qrCodes, 2);
-
         // ビューを作成してPDFを生成
-        // $pdf = PDF::loadView('pdf.qrcodes', compact('qrCodePairs'));
-        // $pdf = PDF::loadView('pdf.pdf-snappy', compact('qrCodes'));
-        $pdf = PDF::loadView('pdf.pdf-preview-table', compact('qrCodes'));
-        // $pdf = PDF::loadView('pdf.pdf-preview-table', compact('qrCodePairs'));
-            // ->setPaper('A4','portrait')
-            // ->setOption('margin-top', 0)
-            // ->setOption('margin-bottom', 0)
-            // ->setOption('margin-left', 0)
-            // ->setOption('margin-right', 0);
+        $pdf = PDF::loadView('pdf.pdf-preview-table', compact('qrCodes'))
+            ->setPaper('A4')
+            ->setOption('margin-left', '16.5mm')
+            ->setOption('margin-right', '16.5mm')
+            ->setOption('margin-top', '10mm')
+            ->setOption('margin-bottom', '10mm')
+            ->setOption('footer-center', '[page] / [topage]') // フッター中央に現在のページ番号と総ページ数を表示
+            ->setOption('footer-font-size', 10);
 
         // return $pdf->download('消耗品QRコード.pdf');
-        return $pdf->setPaper('a4')->inline('document.pdf');
+        return $pdf->inline('消耗品QRコード.pdf');
     }
 
     public function designPDF()
