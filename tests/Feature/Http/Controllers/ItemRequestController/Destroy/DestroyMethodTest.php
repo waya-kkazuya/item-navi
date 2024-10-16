bc2c75b8035dfbfb0a4eff50e3a3ec48ba8c5d5a
waya-kkazuya <k.kazuya.for.application@gmail.com>
2024-10-16 19:56:37 +0900

    test:#124 リクエストの削除のテストを作成

---
+++ tests/Feature/Http/Controllers/ItemRequestController/Destroy/DestroyMethodTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\ItemRequestController\Destroy;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Faker\Factory as FakerFactory;
+use Inertia\Testing\AssertableInertia as Assert;
+use App\Models\User;
+use App\Models\ItemRequest;
+
+class DestroyMethodTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->faker = FakerFactory::create();
+    }
+
+    /** @test */
+    function リクエストが削除できる()
+    {
+        $itemRequest = ItemRequest::factory()->create();
+
+        $user = User::factory()->role(1)->create();
+        $this->actingAs($user);
+
+        $response = $this->delete(route('item_requests.destroy', $itemRequest))
+            ->assertRedirect(route('item_requests.index'));
+
+        $response->assertSessionHas([
+            'message' => 'リクエストを削除しました',
+            'status' => 'danger'
+        ]);
+
+        // データベースの検証
+        $this->assertDatabaseMissing('item_requests', ['id' => $itemRequest->id]);
+    }
+}
