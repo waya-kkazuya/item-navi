995ad6a0a21716fb3ca3ccb22e74ca5a1b4a915c
waya-kkazuya <k.kazuya.for.application@gmail.com>
2024-10-22 20:16:20 +0900

    test:#151 ゲストログイン用のテストを作成

--- tests/Feature/Authorization/GuestAccessTest.php
+++ tests/Feature/Authorization/GuestAccessTest.php
@@ -46,22 +46,60 @@ class GuestAccessTest extends TestCase
     }
 
     /** @test */
-    function ゲストはログイン後の画面にアクセスできない()
+    function ゲストログインユーザーは画面にアクセスできる()
     {   
         $loginUrl = 'login';
-        // $user = User::factory()->role(1)->create();
-        // $this->actingAs($user);
+        // ゲストログインユーザー
+        $user = User::factory()->role(0)->create();
+        $this->actingAs($user);
 
         $item = Item::factory()->create();
 
-        // ゲスト用のリダイレクトのアサ―ト
-        $this->get('dashboard')->assertRedirect($loginUrl);
-        $this->get('items')->assertRedirect($loginUrl);
-        $this->get('items/create')->assertRedirect($loginUrl);
-        $this->from('items/create')->post('items', [])->assertRedirect($loginUrl);
-        $this->get('items/edit')->assertRedirect($loginUrl);
-        $this->from('items/edit')->patch(route('items.update', $item->id), [])->assertRedirect($loginUrl);
-            // ->assertForbidden();
+        // ログインしていないユーザーのリダイレクトのアサ―ト
+        $this->get(route('dashboard'))->assertOk();
+        $this->get(route('items.index'))->assertOk();
+        $this->get(route('items.create'))->assertOk();
+        $this->from(route('items.create'))->post('items', [])->assertRedirect(route('items.create'));
+        $this->get(route('items.edit', ['item' => $item]))->assertOk();
+        $this->from(route('items.edit', ['item' => $item]))
+            ->patch(route('items.update', $item->id), [])->assertRedirect(route('items.edit', ['item' => $item]));
+        $this->from(route('items.show',['item' => $item]))
+            ->delete('items/'.$item->id)->assertRedirect(route('items.index'));
+        
+        $this->get(route('consumable_items'))->assertOk();
+        $this->get(route('inspection_and_disposal_items'))->assertOk();
+        $this->get(route('item_requests.index'))->assertOk();
+        $this->get(route('notifications.index'))->assertOk();
+        $this->get(route('profile.edit'))->assertOk();
+    }
+
+    /** @test */
+    function ゲストログインユーザーはDB保存処理をする処理を実行出来ない()
+    {   
+        $loginUrl = 'login';
+        // ゲストログインユーザー
+        $user = User::factory()->role(0)->create();
+        $this->actingAs($user);
+
+        $item = Item::factory()->create();
+
+        // ログインしていないユーザーのリダイレクトのアサ―ト
+        // DBを変更する機能
+        // ミドルウェアでどう設定するか
+        $this->get(route('items.create'))->assertOk();
+        $this->from(route('items.create'))->post('items', [])->assertRedirect(route('items.create'));
+        $this->get(route('items.edit', ['item' => $item]))->assertOk();
+        $response = $this->from(route('items.edit', ['item' => $item]))
+            ->patch(route('items.update', $item->id), [])->assertRedirect(route('items.edit', ['item' => $item]));
+        $response = $this->followRedirects($response);
+
+        // dd($response);
+        // フラッシュメッセージの内容をアサート
+        $response->assertInertia(fn (Assert $page) => $page
+            ->component('Items/Edit') // コンポーネント名を指定
+            ->where('flash.message', 'ゲストには許可されていません、ログインして実行してください') // フラッシュメッセージを指定
+        );
+
         $this->from('items/show')->delete('items/' . $item->id)->assertRedirect($loginUrl);
         
         $this->get('consumable_items')->assertRedirect($loginUrl);
@@ -69,7 +107,99 @@ class GuestAccessTest extends TestCase
         $this->get('item-requests')->assertRedirect($loginUrl);
         $this->get('notifications')->assertRedirect($loginUrl);
         $this->get('profile')->assertRedirect($loginUrl);
-
     }
+
+    /** @test */
+    function ゲストログインユーザーは備品の新規登録が許可されない()
+    {   
+        // ゲストログインユーザー
+        $user = User::factory()->role(0)->create();
+        $this->actingAs($user);
+
+        $item = Item::factory()->create();
+
+        // ログインしていないユーザーのリダイレクトのアサ―ト
+        // DBを変更する機能
+        // ミドルウェアでどう設定するか
+        $this->get(route('items.create'))->assertOk();
+        $response = $this->from(route('items.create'))
+            ->post('items', [])->assertRedirect(route('items.create'));
+        $response = $this->followRedirects($response);
+
+        // フラッシュメッセージの内容をアサート
+        $response->assertInertia(fn (Assert $page) => $page
+            ->component('Items/Create') // コンポーネント名を指定
+            ->has('flash.message')
+            ->where('flash.message', 'ゲストには許可されていません、ログインして実行してください')
+            ->has('flash.status')
+            ->where('flash.status', 'danger')
+        );
+    }
+
+    /** @test */
+    function ゲストログインユーザーは備品の編集更新が許可されない()
+    {   
+        // ゲストログインユーザー
+        $user = User::factory()->role(0)->create();
+        $this->actingAs($user);
+
+        $item = Item::factory()->create();
+
+        // ログインしていないユーザーのリダイレクトのアサ―ト
+        $this->get(route('items.edit', ['item' => $item]))->assertOk();
+        $response = $this->from(route('items.edit', ['item' => $item]))
+            ->patch(route('items.update', $item->id), [])->assertRedirect(route('items.edit', ['item' => $item]));
+        $response = $this->followRedirects($response);
+
+        // フラッシュメッセージの内容をアサート
+        $response->assertInertia(fn (Assert $page) => $page
+            ->component('Items/Edit')
+                ->has('flash.message')
+                ->where('flash.message', 'ゲストには許可されていません、ログインして実行してください')
+                ->has('flash.status')
+                ->where('flash.status', 'danger')
+        );
+    }
+
+    /** @test */
+    function ゲストログインユーザーは備品の復元が許可されない()
+    {   
+        // ゲストログインユーザー
+        $user = User::factory()->role(0)->create();
+        $this->actingAs($user);
+
+        // アイテムを作成し、ソフトデリートする
+        $item = Item::factory()->create();
+        $item->delete();
+
+        // アイテムがソフトデリートされたことを確認
+        $this->assertSoftDeleted('items', ['id' => $item->id]);
+
+        // アイテムを復元するリクエストを送信
+        $response = $this->from(route('items.index'))
+            ->post(route('items.restore', $item->id));
+
+        // レスポンスが期待通りか確認
+        $response->assertStatus(302);
+        $response->assertRedirect(route('items.index'));
+        $response = $this->followRedirects($response);
+
+        // フラッシュメッセージの内容をアサート
+        $response->assertInertia(fn (Assert $page) => $page
+            ->component('Items/Index')
+                ->has('flash.message')
+                ->where('flash.message', 'ゲストには許可されていません、ログインして実行してください')
+                ->has('flash.status')
+                ->where('flash.status', 'danger')
+        );
+
+        // アイテムが復元されていないことを確認
+        $this->assertSoftDeleted('items', ['id' => $item->id]);
+    }
+
+
+    // 廃棄モーダルによる廃棄、点検モーダルによる点検
+    // 入出庫モーダルによる入庫、出庫
+    // リクエストの新規登録
 
 }
