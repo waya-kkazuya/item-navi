a8836814ab43f94b6ea04395ae60c7be1b15c4e0
waya-kkazuya <k.kazuya.for.application@gmail.com>
2024-10-22 17:42:51 +0900

    fix:#151 Guset用ミドルウェアを作成する前にUser用のミドルウェアの名前を修正

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,6 +65,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'checkRole' => \App\Http\Middleware\CheckRole::class,
+        'CheckRoleUser' => \App\Http\Middleware\CheckRoleUser::class,
     ];
 }
