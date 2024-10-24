8f4c614376348940d7b922ba61bf81f23312aa70
waya-kkazuya <k.kazuya.for.application@gmail.com>
2024-10-24 14:19:45 +0900

    fix:#160 トランザクション処理のcatch節のテストコードの書き方が分からないのでひとまずコメントアウト

--- tests/Feature/Http/Controllers/ProfileController/UpdateMethodTest.php
+++ tests/Feature/Http/Controllers/ProfileController/UpdateMethodTest.php
@@ -33,6 +33,9 @@ use Intervention\Image\ImageManager;
 // use Intervention\Image\Drivers\Gd\Driver;
 use Intervention\Image\Drivers\Imagick\Driver;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
 
 class UpdateMethodTest extends TestCase
 {
@@ -94,6 +97,163 @@ class UpdateMethodTest extends TestCase
         $this->assertSame('mocked_profile_image.jpg', $user->profile_image);
     }
 
+    // /** @test */
+    // function ProfileControllerでトランザクション処理が失敗する場合()
+    // {
+
+    //     // トランザクションの開始をモック
+    //     // DB::shouldReceive('beginTransaction')->once();
+    //     // DB::shouldReceive('commit')
+    //     //     ->once()
+    //     //     ->andThrow(\Exception::class, 'トランザクションのコミットに失敗しました');
+    //     $partialMock = DB::partialMock();
+    //     // $partialMock->shouldReceive('commit')
+    //     //     ->andThrow(new \Exception('トランザクションのコミットに失敗しました'));
+    //     $partialMock->shouldReceive('commit')->never();
+    //     // DB::shouldReceive('rollback')->once();
+    //     // $this->withoutExceptionHandling();
+
+    //     // フェイクの画像ファイルを作成
+    //     $this->fakeImage = UploadedFile::fake()->image('test_profile_image.jpg');
+
+    //     // ImageServiceのモックを作成
+    //     $this->imageService = Mockery::mock(ImageService::class);
+    //     $this->imageService->shouldReceive('profileImageResizeUpload')
+    //         ->once()
+    //         ->with(Mockery::on(function ($arg) {
+    //             return $arg instanceof UploadedFile && $arg->getClientOriginalName() === 'test_profile_image.jpg';
+    //         }))
+    //         ->andReturn('mocked_profile_image.jpg');
+    //     // サービスコンテナにモックを登録
+    //     $this->app->instance(ImageService::class, $this->imageService);
+
+    //     $user = User::factory()->role(1)->create([
+    //         'name' => 'BeforeTestUser',
+    //         'profile_image' => 'before_profile_image.jpg'
+    //     ]);
+
+    //     Storage::fake('public');
+    //     // 元々の画像ファイルをストレージに追加
+    //     Storage::disk('public')->put('profile/before_profile_image.jpg', 'dummy content');
+
+    //     try {
+    //         $response = $this->actingAs($user)
+    //             ->patch('/profile', [
+    //                 'name' => 'TestUser',
+    //                 'profile_image_file' => $this->fakeImage
+    //             ]);
+
+            
+    //         // Mockeryのスパイを使ってメソッドが呼ばれたことを検証
+    //         // Mockery::spy(User::class)->shouldHaveReceived('save')->once();   
+    //         // dd($response);
+
+    //         $response->assertStatus(500);
+
+    //     // commitメソッドが呼ばれたことをスパイで検証
+    //     // DB::shouldHaveReceived('commit')->once();
+
+    //             // $response->assertSessionHasNoErrors()
+    //             //     ->assertRedirect('/profile');
+
+
+    //          // テストがここまで来るべきではないので失敗させる
+    //         // $this->fail('Expected Exception not thrown.');
+    //     } catch (\Exception $e) {
+    //         // dd($e->getMessage());
+    //         // 例外が発生したことを確認
+    //         // $this->assertEquals('トランザクションの強制失敗', $e->getMessage());
+        
+    //     }
+
+    //     // フラッシュメッセージを確認
+    //     // $response->assertSessionHas('message', 'プロフィールの更新中にエラーが発生しました');
+    //     // $response->assertSessionHas('status', 'danger');
+
+
+    //     // dd(session('errors'));
+    //     // レスポンスの内容をダンプして確認
+    //     // dd($response->getContent());
+
+    //     // 500エラーが返ることを確認
+    //     // $response->assertStatus(500);
+
+    //     // プロフィール画像の削除が行われたかを確認
+    //     Storage::disk('public')->assertMissing('profile/mocked_profile_image.jpg');
+    //     Storage::disk('public')->assertExists('profile/before_profile_image.jpg');
+        
+    //     $this->assertDatabaseHas('users', [
+    //         'name' => 'BeforeTestUser',
+    //         'profile_image' => 'before_profile_image.jpg'
+    //     ]);
+
+    //     // $user->refresh();
+    //     // $this->assertSame('Test User', $user->name);
+    //     // $this->assertSame('mocked_profile_image.jpg', $user->profile_image);
+
+    //     Mockery::close();
+    // }
+
+    // /** @test */
+    // function ProfileControllerでロールバックした時の処理のテスト()
+    // {
+    //     // Logファサードをモック
+    //     $partialMock = Log::partialMock();
+    //     $partialMock->shouldReceive('info')
+    //         ->once()
+    //         ->andThrow(new \Exception('Logging error for testing purposes'));
+
+    //     // フェイクの画像ファイルを作成
+    //     $this->fakeImage = UploadedFile::fake()->image('test_profile_image.jpg');
+
+    //     // ImageServiceのモックを作成
+    //     $this->imageService = Mockery::mock(ImageService::class);
+    //     $this->imageService->shouldReceive('profileImageResizeUpload')
+    //         ->once()
+    //         ->with(Mockery::on(function ($arg) {
+    //             return $arg instanceof UploadedFile && $arg->getClientOriginalName() === 'test_profile_image.jpg';
+    //         }))
+    //         ->andReturn('mocked_profile_image.jpg');
+    //     // サービスコンテナにモックを登録
+    //     $this->app->instance(ImageService::class, $this->imageService);
+
+    //     $user = User::factory()->role(1)->create([
+    //         'name' => 'BeforeTestUser',
+    //         'profile_image' => null
+    //     ]);
+
+    //     $response = $this->actingAs($user)
+    //         ->patch('/profile', [
+    //             'name' => 'TestUser',
+    //             'profile_image_file' => $this->fakeImage
+    //         ]);
+
+    //     $response->assertStatus(500);
+
+    //     // フラッシュメッセージを確認
+    //     $response->assertSessionHas('message', 'プロフィールの更新中にエラーが発生しました');
+    //     $response->assertSessionHas('status', 'danger');
+
+
+    //     // dd(session('errors'));
+    //     // レスポンスの内容をダンプして確認
+    //     // dd($response->getContent());
+
+    //     $this->assertDatabaseHas('users', [
+    //         'name' => 'BeforeTestUser',
+    //         'profile_image' => 'before_profile_image.jpg'
+    //     ]);
+
+    //     // $user->refresh();
+    //     // $this->assertSame('Test User', $user->name);
+    //     // $this->assertSame('mocked_profile_image.jpg', $user->profile_image);
+    // }
+
+
+
+
+
+    // デフォルトで用意されていたテスト
     // public function test_email_verification_status_is_unchanged_when_the_email_address_is_unchanged(): void
     // {
     //     $user = User::factory()->create();
