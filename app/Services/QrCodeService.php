3e6ae825fbeb476269dbdb21a5e8173ef0d3d39a
waya-kkazuya <k.kazuya.for.application@gmail.com>
2024-10-29 13:49:55 +0900

    fix:#143 QrCodeServiceにログ設置、不要なメソッドを削除

--- app/Services/QrCodeService.php
+++ app/Services/QrCodeService.php
@@ -9,58 +9,60 @@ use Intervention\Image\Encoders\JpegEncoder;
 use SimpleSoftwareIO\QrCode\Facades\QrCode;
 use Illuminate\Support\Facades\Storage;
 use Intervention\Image\Typography\FontFactory;
+use Illuminate\Support\Facades\Log;
 
 class QrCodeService
 {
-  public static function upload($item) {
-    // $item->idでQRコードを生成する
-    $qrCode = QrCode::format('png')->size(300)->generate($item->id);
-    // QRコードの名前はQR-とランダムな文字列
-    $qrCodeNameToStore = 'QR-' . uniqid(rand().'_') . '.png';
-    Storage::disk('public')->put('qrcode/'.$qrCodeNameToStore, $qrCode);
-    
-    // 保存したファイルのパスを取得
-    $qrCodefilePath = Storage::disk('public')->path('qrcode/'.$qrCodeNameToStore);
-
-    $qrManager = new ImageManager(new Driver());
-    \Log::info($qrCodefilePath);
-    $qrImage = $qrManager->read($qrCodefilePath);
-
-    $label = $qrManager->create(910, 550)->fill('fff');
-    $label->place($qrImage, 'top-left', 40, 125);
-
-    // 白地に文字を追加
-    $label->text('管理ID'.$item->management_id, 370, 160, function(FontFactory $font) {
-        $font->filename(public_path('storage/fonts/NotoSansJP-Medium.ttf'));
-        $font->size(30);
-        $font->color('#000');
-    });
-    
-    // $lineBreakText = mb_convert_encoding(self::wrapText('備品名 '.$item->name, 14), 'UTF-8'); 
-    $label->text('備品名 '.$item->name, 370, 230, function(FontFactory $font) {
-        $font->filename(public_path('storage/fonts/NotoSansJP-Medium.ttf'));
-        $font->size(30);
-        $font->color('#000');
-    });
-
-    $label->text('カテゴリ'.$item->category->name, 370, 300, function(FontFactory $font) {
-        $font->filename(public_path('storage/fonts/NotoSansJP-Medium.ttf'));
-        $font->size(30);
-        $font->color('#000');
-    });
-
-    $labelNameToStore = 'label-'.uniqid(rand().'_') .'.jpg';
-    Storage::disk('public')->put('labels/'.$labelNameToStore, $label->encode(new JpegEncoder()));
-
-    // トランザクション処理が失敗した時のロールバック用にQRコード画像名も返す
-    return [
-      'labelNameToStore' => $labelNameToStore,
-      'qrCodeNameToStore' => $qrCodeNameToStore,
-    ];
-  }
-
-  public static function wrapText($text, $maxLength)
+  public static function upload($item)
   {
-      return wordwrap($text, $maxLength, PHP_EOL, true);
+    Log::info('QrCodeService upload method called');
+
+    try {
+      // $item->idを元にQRコードを生成する
+      $qrCode = QrCode::format('png')->size(300)->generate($item->id);
+      $qrCodeNameToStore = 'QR-' . uniqid(rand().'_') . '.png';
+      Storage::disk('public')->put('qrcode/'.$qrCodeNameToStore, $qrCode);
+      $qrCodefilePath = Storage::disk('public')->path('qrcode/'.$qrCodeNameToStore);
+
+      $qrManager = new ImageManager(new Driver());
+      $qrImage = $qrManager->read($qrCodefilePath);
+
+      $label = $qrManager->create(910, 550)->fill('fff'); //白地に画像と文字列を合成
+      $label->place($qrImage, 'top-left', 40, 125);
+
+      $label->text('管理ID'.$item->management_id, 370, 160, function(FontFactory $font) {
+          $font->filename(public_path('storage/fonts/NotoSansJP-Medium.ttf'));
+          $font->size(30);
+          $font->color('#000');
+      });
+      $label->text('備品名 '.$item->name, 370, 230, function(FontFactory $font) {
+          $font->filename(public_path('storage/fonts/NotoSansJP-Medium.ttf'));
+          $font->size(30);
+          $font->color('#000');
+      });
+      $label->text('カテゴリ'.$item->category->name, 370, 300, function(FontFactory $font) {
+          $font->filename(public_path('storage/fonts/NotoSansJP-Medium.ttf'));
+          $font->size(30);
+          $font->color('#000');
+      });
+
+      $labelNameToStore = 'label-'.uniqid(rand().'_') .'.jpg';
+      Storage::disk('public')->put('labels/'.$labelNameToStore, $label->encode(new JpegEncoder()));
+
+      Log::info('QrCodeService upload method succeed');
+
+      // トランザクション処理が失敗した時のロールバック用にQRコード画像名も返す
+      return [
+        'labelNameToStore' => $labelNameToStore,
+        'qrCodeNameToStore' => $qrCodeNameToStore,
+      ];
+    } catch (\Exception $e) {
+      Log::error('QrCodeService upload method failed', [
+        'error' => $e->getMessage(),
+        'stack' => $e->getTraceAsString(),
+      ]);
+
+      throw $e; // エラーを再スローしてコントローラでキャッチできるように
+    }
   }
 }
