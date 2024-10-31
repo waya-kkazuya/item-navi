8faf6dad8c617a479e038c11a3dc56cd157b8357
waya-kkazuya <k.kazuya.for.application@gmail.com>
2024-10-31 14:27:18 +0900

    fix:#221 Locationモデルの不要なモデルのuse分を削除

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Item;
-use App\Models\Wish;
 
 class Location extends Model
 {
