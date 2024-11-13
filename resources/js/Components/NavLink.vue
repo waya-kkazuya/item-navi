5713fe642705153b157654eddfeb10390a743f02
waya-kkazuya <k.kazuya.for.application@gmail.com>
2024-11-13 14:48:58 +0900

    feat:#243 Components/NavLink.vueにTypeScriptを適用

--- resources/js/Components/NavLink.vue
+++ resources/js/Components/NavLink.vue
@@ -1,18 +1,17 @@
-<script setup>
+<script setup lang="ts">
 import { computed } from 'vue';
 import { Link } from '@inertiajs/vue3';
+import type { ComputedRef } from 'vue';
 
-const props = defineProps({
-    href: {
-        type: String,
-        required: true,
-    },
-    active: {
-        type: Boolean,
-    },
-});
 
-const classes = computed(() =>
+type Props = {
+    href: string;
+    active?: boolean;
+};
+
+const props = defineProps<Props>();
+
+const classes: ComputedRef<string> = computed(() =>
     props.active
         ? 'inline-flex items-center px-1 pt-1 border-b-2 border-white text-sm font-medium leading-5 text-white focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
         : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-300 hover:text-gray-100 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out'
