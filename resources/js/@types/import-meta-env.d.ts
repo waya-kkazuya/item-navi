a34b27aaee312aca25650b311e69096a0b101e6e
waya-kkazuya <k.kazuya.for.application@gmail.com>
2024-11-15 14:50:42 +0900

    fix #243 ImportMetaの型宣言ファイルの名前をenv.d.tsからimport-meta-env.d.tsに変更

---
+++ resources/js/@types/import-meta-env.d.ts
@@ -0,0 +1,14 @@
+export {};
+
+declare {
+    interface ImportMetaEnv {
+        readonly VITE_APP_NAME: string;
+        readonly VITE_API_BASE_URL: string;
+        readonly VITE_APP_URL: string;
+    }
+    
+    interface ImportMeta {
+        readonly env: ImportMetaEnv;
+        readonly glob: (pattern: string) => Record<string, () => Promise<unknown>>;
+    }
+}
