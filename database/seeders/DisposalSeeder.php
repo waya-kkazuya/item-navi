e4afebf485fbb021d0e5e314d6e814ae29015193
waya-kkazuya <k.kazuya.for.application@gmail.com>
2024-12-05 20:32:57 +0900

    feat:#298 リアルな感じのダミーデータを登録するためのSeederファイル作成

---
+++ database/seeders/DisposalSeeder.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+
+class DisposalSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('disposals')->insert([
+            [
+                'id' => 1,
+                'item_id' => 11,
+                'disposal_scheduled_date' => NULL,
+                'disposal_date' => '2024-10-15',
+                'disposal_person' => '管理者大川',
+                'details' => '壊れたため、廃棄します。',
+                'created_at' => Carbon::createFromFormat('Y/m/d H:i:s', '2024/10/15 15:04:00'),
+                'updated_at' => Carbon::createFromFormat('Y/m/d H:i:s', '2024/10/15 15:52:00'),
+            ],
+            [
+                'id' => 2,
+                'item_id' => 26,
+                'disposal_scheduled_date' => NULL,
+                'disposal_date' => '2024-10-15',
+                'disposal_person' => '管理者大川',
+                'details' => '壊れたため、廃棄します。',
+                'created_at' => Carbon::createFromFormat('Y/m/d H:i:s', '2024/10/15 15:05:00'),
+                'updated_at' => Carbon::createFromFormat('Y/m/d H:i:s', '2024/10/15 15:53:00'),
+            ],
+            [
+                'id' => 3,
+                'item_id' => 73,
+                'disposal_scheduled_date' => '2025-02-05',
+                'disposal_date' => NULL,
+                'disposal_person' => NULL,
+                'details' => NULL,
+                'created_at' => Carbon::createFromFormat('Y/m/d H:i:s', '2024/10/01 15:10:00'),
+                'updated_at' => Carbon::createFromFormat('Y/m/d H:i:s', '2024/10/01 15:10:00'),
+            ],
+            [
+                'id' => 4,
+                'item_id' => 88,
+                'disposal_scheduled_date' => '2026-03-25',
+                'disposal_date' => NULL,
+                'disposal_person' => NULL,
+                'details' => NULL,
+                'created_at' => Carbon::createFromFormat('Y/m/d H:i:s', '2024/10/01 15:11:00'),
+                'updated_at' => Carbon::createFromFormat('Y/m/d H:i:s', '2024/10/01 15:11:00'),
+            ],
+        ]);
+    }
+}
--- database/seeders/EditReasonSeeder.php
+++ database/seeders/EditReasonSeeder.php
@@ -15,13 +15,32 @@ class EditReasonSeeder extends Seeder
     {
         DB::table('edit_reasons')->insert([
             [
-                'reason' => '入力内容に変更があったため'
+                'reason' => '新しい情報の追加のため'
             ],
             [
                 'reason' => '入力内容にミスがあったため'
             ],
             [
-                'reason' => '新しく判明した情報があったため'
+                'reason' => '利用状況が変わったため'
+            ],
+            [
+                'reason' => '使用者が変わったため'
+            ],
+            [
+                'reason' => '利用場所が変わったため'
+            ],
+            [
+                'reason' => '保管場所が変わったため'
+            ],
+            [
+                'reason' => '点検の予定日が決まったため'
+            ],
+            [
+                'reason' => '廃棄の予定日が決まったため'
+            ],
+            [
+                'reason' => '備品の状態に変化があったため
+'
             ],
             [
                 'reason' => 'その他（理由詳細に記載）'
