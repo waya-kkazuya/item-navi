736d670b5ea5abdc21c03f9df8344bc1ac6be07e
waya-kkazuya <k.kazuya.for.application@gmail.com>
2024-12-09 16:38:06 +0900

    fix:#270 Dashboard.vueのheader部分とmain部分の余白を修正

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -68,7 +68,7 @@ const toggleDetails = (history: EditHistoryType) => {
             </h2>
         </template>
 
-        <div class="py-12">
+        <div class="py-2 md:py-4">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="md:flex w-full p-4">
