b03e4eb552706811a373606597c3f0e60b7d5730
waya-kkazuya <k.kazuya.for.application@gmail.com>
2024-12-09 15:16:45 +0900

    fix:#314 Item/Edit.vueの「更新する」ボタンのCSS修正

--- resources/js/Pages/Items/Edit.vue
+++ resources/js/Pages/Items/Edit.vue
@@ -366,7 +366,7 @@ const clearDisposalSchedule = () => {
                                                         </div>
                                                     </div>
                                                     <div class="p-2 w-full">
-                                                        <button id="updateButton" class="flex mx-auto text-white text-xs md:text-base bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded">
+                                                        <button id="updateButton" class="flex mx-auto text-white text-xs md:text-sm bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded">
                                                             更新する
                                                         </button>
                                                     </div>
