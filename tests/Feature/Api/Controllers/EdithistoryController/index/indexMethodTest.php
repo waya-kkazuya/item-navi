67d0ea67bfd1028dbd1808d1d702cae5dc3ad63d
waya-kkazuya <k.kazuya.for.application@gmail.com>
2024-12-11 13:28:52 +0900

    fix:#263 EdithistoryControllerのテストの不要な部分を削除

--- tests/Feature/Api/Controllers/EdithistoryController/index/indexMethodTest.php
+++ tests/Feature/Api/Controllers/EdithistoryController/index/indexMethodTest.php
@@ -3,36 +3,12 @@
 namespace Tests\Feature\Api\Controllers\EdithistoryController\index;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Faker\Factory as FakerFactory;
 use Tests\TestCase;
 use App\Models\User;
 use App\Models\Item;
-use App\Models\Unit;
-use App\Models\Category;
-use App\Models\Location;
-use App\Models\UsageStatus;
-use App\Models\AcquisitionMethod;
 use App\Models\Edithistory;
-use App\Models\Inspection;
-use App\Models\EditReason;
-use App\Models\RequestStatus;
-use App\Models\StockTransaction;
-use App\Services\ImageService;
-use Faker\Factory as FakerFactory;
-use Inertia\Testing\AssertableInertia as Assert;
-use Mockery;
-use App\Services\ManagementIdService;
-use Illuminate\Support\Facades\Auth;
-use Carbon\Carbon;
-use Illuminate\Database\Console\DumpCommand;
-use Illuminate\Testing\Fluent\AssertableJson;
-use Inertia\Inertia;
-use Illuminate\Http\UploadedFile;
-use Illuminate\Support\Facades\Storage;
-use Intervention\Image\ImageManager;
-// use Intervention\Image\Drivers\Gd\Driver;
-use Intervention\Image\Drivers\Imagick\Driver;
-use Illuminate\Support\Facades\Session;
 
 class indexMethodTest extends TestCase
 {
@@ -50,8 +26,7 @@ class indexMethodTest extends TestCase
     function 編集履歴モーダル用のデータをAPIで取得できる()
     {
         // 世界の構築
-        // adminユーザーを作成
-        $user = User::factory()->role(1)->create();
+        $user = User::factory()->role(1)->create(); // adminユーザーを作成
         $this->actingAs($user);   
 
         $item = Item::factory()->create();
@@ -63,12 +38,12 @@ class indexMethodTest extends TestCase
         //     'X-Inertia' => 'true',
         //     'X-Inertia-Version' => Inertia::getVersion(),
         // ])->get('/api/edithistory?item_id=' . $item->id);
+
         $response = $this->get('/api/edithistory?item_id=' . $item->id);
         // $response = $this->get('/api/edithistory', ['item_id' => $item->id]);
         $response->assertOk();
         // dd($response->json());
-
-        // dd($response);
+        
         $response->assertJson(fn (AssertableJson $json) =>
             $json->has('edithistories',10)
                 ->has('edithistories', fn ($json) =>
