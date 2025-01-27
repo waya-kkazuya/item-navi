21185db50ec66b9ba28e0edc82e1a3caee9fc61e
waya-kkazuya <blackcatandbluesky@gmail.com>
2025-01-27 15:24:22 +0900

    #387: 開発環境では成功したので本番環境にも実装

--- config/snappy.php
+++ config/snappy.php
@@ -35,7 +35,7 @@ return [
     
     'pdf' => [
         'enabled' => true,
-        'binary' => base_path('vendor\wemersonjanuario\wkhtmltopdf-windows\bin\64bit\wkhtmltopdf'),
+        'binary' => env('WKHTMLTOPDF_BINARY', '/usr/local/bin/wkhtmltopdf'),
         'timeout' => false,
         'options' => [],
         'env'     => [],
