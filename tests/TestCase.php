23e8b7333600d557c17cf11d1322f0e961bfb59b
waya-kkazuya <blackcatandbluesky@gmail.com>
2025-02-04 16:33:42 +0900

    CICD:#223 DISABLE_CSRFの値で、CI環境ではcsrfを無効にする

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,14 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        
+        // 環境変数に基づいてCSRFミドルウェアを無効にするかどうかを決定
+        if (env('DISABLE_CSRF', false)) {
+            $this->withoutMiddleware(\Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class);
+        }
+    }
 }
