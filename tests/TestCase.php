20ad9700523e431ba1e0d3b1b1622c6349f460f1
waya-kkazuya <blackcatandbluesky@gmail.com>
2025-02-05 22:19:36 +0900

    CICD:#223 TestCaseの不要なコードを削除

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,15 +7,4 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
-
-    // protected function setUp(): void
-    // {
-    //     parent::setUp();
-        
-    //     // 環境変数に基づいてCSRFミドルウェアを無効にするかどうかを決定
-    //     if (env('DISABLE_CSRF', false)) {
-    //         \Log::info('CSRFミドルウェア無効化');
-    //         $this->withoutMiddleware(\Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class);
-    //     }
-    // }
 }
