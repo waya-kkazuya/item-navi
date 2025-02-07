98df6bdd9f4a363874340bb69658336cf8bde4a4
waya-kkazuya <blackcatandbluesky@gmail.com>
2025-02-07 18:58:43 +0900

    fix:#315 Middlewareをフォーマットする

--- app/Http/Middleware/RestrictGuestAccess.php
+++ app/Http/Middleware/RestrictGuestAccess.php
@@ -4,8 +4,9 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
 
 class RestrictGuestAccess
 {
@@ -17,20 +18,20 @@ class RestrictGuestAccess
     public function handle(Request $request, Closure $next): Response
     {
         if (Auth::check() && Auth::user()->role == 0) {
-            \Log::info("RestrictGuestAccessミドルウェア");
+            Log::info("RestrictGuestAccessミドルウェア");
 
             if ($request->expectsJson()) {
-                \Log::info("RestrictGuestAccessミドルウェア:APIリクエスト");
+                Log::info("RestrictGuestAccessミドルウェア:APIリクエスト");
                 return response()->json([
                     'message' => 'ゲストには許可されていない機能です、ログインして実行してください',
-                    'status' => 'danger'
+                    'status'  => 'danger',
                 ], 403);
             }
 
             return redirect()->back()
                 ->with([
                     'message' => 'ゲストには許可されていない機能です、ログインして実行してください',
-                    'status' => 'danger'
+                    'status'  => 'danger',
                 ]);
         }
         return $next($request);
