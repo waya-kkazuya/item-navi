78f98635e6c4d20903dfd6d23755e5c8513d4b57
waya-kkazuya <blackcatandbluesky@gmail.com>
2025-03-06 16:57:02 +0900

    fix:#321  #426 点検と廃棄の通知がartisanコマンドで来るよう修正

--- database/seeders/DisposalSeeder.php
+++ database/seeders/DisposalSeeder.php
@@ -37,7 +37,7 @@ class DisposalSeeder extends Seeder
             [
                 'id'                      => 3,
                 'item_id'                 => 73,
-                'disposal_scheduled_date' => '2025-03-27',
+                'disposal_scheduled_date' => '2025-03-20',
                 'disposal_date'           => null,
                 'disposal_person'         => null,
                 'details'                 => null,
