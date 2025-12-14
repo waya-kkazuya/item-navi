fdda697ef2fdde2b75f93e6a075591ad2f5e6db6
waya-kkazuya <k.kazuya.for.application@gmail.com>
2025-12-14 16:27:22 +0900

    fix:#505 プロフィール画像URL取得時のstorageの設定を修正

--- app/Http/Controllers/Api/ProfileController.php
+++ app/Http/Controllers/Api/ProfileController.php
@@ -13,9 +13,8 @@ class ProfileController extends Controller
         $user = Auth::user();
         return response()->json([
             'profile_image_url' => $user->profile_image
-                ? Storage::disk('s3')->url('profile/' . $user->profile_image)
-                : Storage::disk('s3')->url('profile/profile_default_image.png'),
+                ? Storage::disk()->url('profile/' . $user->profile_image)
+                : Storage::disk()->url('profile/profile_default_image.png'),
         ]);
     }
 }
-
