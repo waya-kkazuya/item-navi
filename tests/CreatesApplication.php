a815135009450765b11796ac09fb74c6c8705b41
Kazuya <blackcatandbluesky@gmail.com>
2025-12-30 16:54:19 +0900

    fix: update test application bootstrapping for Laravel 11

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -2,6 +2,7 @@
 
 namespace Tests;
 
+use Illuminate\Contracts\Console\Kernel;
 use Illuminate\Foundation\Application;
 
 trait CreatesApplication
@@ -10,7 +11,7 @@ trait CreatesApplication
     {
         $app = require __DIR__.'/../bootstrap/app.php';
 
-        $app->boot();
+        $app->make(Kernel::class)->bootstrap();
 
         return $app;
     }
